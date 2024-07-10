b53b32e338a88f6deb89e075d8eb9d5d923acd74
SaurabhJais26 <sjsaurabhjais@gmail.com>
2024-07-10 18:09:35 +0530

    updated login view view model and added common button

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -8,37 +8,30 @@
 import SwiftUI
 
 struct LoginView: View {
-    
-    @State var email = ""
-    @State var password = ""
+    @StateObject var viewModel = LoginViewViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
                 // Header
-                HeaderView()
+                HeaderView(title: "To Do List", subtitle: "Get things done", angle: 15, background: .pink)
                 
                 // Login Form
                 Form {
-                    TextField("Email Address", text: $email)
+                    TextField("Email Address", text: $viewModel.email)
                         .textFieldStyle(DefaultTextFieldStyle())
-                    SecureField("Password", text: $password)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                    SecureField("Password", text: $viewModel.password)
                         .textFieldStyle(DefaultTextFieldStyle())
                     
-                    Button {
+                    TLButton(title: "Log In", background: .blue) {
+                        // Attempt Log In
                         
-                    }label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundColor(Color.blue)
-                                
-                                Text("Log In")
-                                    .foregroundColor(Color.white)
-                                    .bold()
-                            }
-                        }
+                    }
                     .padding()
                 }
+                .offset(y: -50)
                 
                 // Create Account
                 VStack {
