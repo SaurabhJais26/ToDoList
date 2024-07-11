e00733f2adb4b980d34824b9b71dc9a50cdc667e
SaurabhJais26 <sjsaurabhjais@gmail.com>
2024-07-11 17:29:46 +0530

    added new item save functionality in database.

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     @Published var title = ""
@@ -15,7 +17,30 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
+        // get current userid
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        // create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
         
+        // save model in db
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     var canSave: Bool {
         guard !title.trimmingCharacters(in: .whitespaces).isEmpty else {
