8d31f11b63b26206047876af28fb421c18afcc22
SaurabhJais26 <sjsaurabhjais@gmail.com>
2024-07-12 12:48:24 +0530

    profile screen completed and logout func too.

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -6,8 +6,25 @@
 //  ViewModel for single to do item view (each row in items list)
 
 import Foundation
-
+import FirebaseAuth
+import FirebaseFirestore
 
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
